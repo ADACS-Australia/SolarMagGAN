472227a6454a99536f9005b89f28cd8dfbe8e79c
Cam <camerontasmith@gmail.com>
2019-07-03 12:36:18 +1000

    added image resize script

---
+++ image_resize.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import argparse
+import os
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    '--dir', type=str, required=True, help='directory to process')
+parser.add_argument('--h', type=int, default=1024, help='target height')
+parser.add_argument('--w', type=int, default=1024, help='target width')
+opts = parser.parse_args()
+
+for fname in os.listdir(opts.dir):
+    fname = opts.dir + "/" + fname
+    Image.open(fname).resize((opts.w, opts.h), Image.LANCZOS).save(fname)
