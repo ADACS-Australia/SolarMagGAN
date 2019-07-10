5753eb43ba5b8eb2546b50338cff36fa79e9b04f
Cam <camerontasmith@gmail.com>
2019-07-10 12:57:08 +1000

    added comments

--- test.py
+++ test.py
@@ -27,8 +27,6 @@ def listdir_nohidden(path):
     return glob.glob(os.path.join(path, '*'))
 
 
-# %%
-
 SLEEP_TIME = 1000
 DISPLAY_ITER = 2
 MAX_ITER = 6
@@ -41,16 +39,14 @@ INPUT1 = 'TEST_INPUT1'  # testing input with testing output (near side data)
 INPUT2 = 'TEST_INPUT2'  # testing input without testing output (far side data)
 OUTPUT = 'TEST_OUTPUT'  # testing output for INPUT1
 
-ISIZE = 1024
-NC_IN = 1
-NC_OUT = 1
-BATCH_SIZE = 1
+ISIZE = 1024  # input size
+NC_IN = 1  # number of input channels
+NC_OUT = 1  # number of output channels
+BATCH_SIZE = 1  # batch size
 
 RSUN = 392
 SATURATION = 100
 THRESHOLD = 10
-
-#%%
 
 OP1 = INPUT1 + '_to_' + OUTPUT
 OP2 = INPUT2 + '_to_' + OUTPUT
@@ -63,6 +59,7 @@ IMAGE_LIST1 = sorted(glob.glob(IMAGE_PATH1))
 IMAGE_LIST2 = sorted(glob.glob(IMAGE_PATH2))
 IMAGE_LIST3 = sorted(glob.glob(IMAGE_PATH3))
 
+# file path for the results
 RESULT_PATH_MAIN = './RESULTS/' + TRIAL_NAME + '/'
 os.mkdir(RESULT_PATH_MAIN) if not os.path.exists(RESULT_PATH_MAIN) else None
 
@@ -75,7 +72,6 @@ os.mkdir(RESULT_PATH2) if not os.path.exists(RESULT_PATH2) else None
 FIGURE_PATH_MAIN = './FIGURES/' + TRIAL_NAME + '/'
 os.mkdir(FIGURE_PATH_MAIN) if not os.path.exists(FIGURE_PATH_MAIN) else None
 
-#%%
 
 def SCALE(DATA, RANGE_IN, RANGE_OUT):
 
@@ -89,30 +85,35 @@ def SCALE(DATA, RANGE_IN, RANGE_OUT):
         if (DOMAIN[1] - DOMAIN[0]) != 0:
             B = DOMAIN[1] - DOMAIN[0]
         else:
-            B =  1.0 / DOMAIN[1]
+            B = 1.0 / DOMAIN[1]
         return (X - DOMAIN[0]) / B
 
     return INTERP(UNINTERP(DATA))
+
 
 def TUMF_VALUE(IMAGE, RSUN, SATURATION, THRESHOLD):
     VALUE_POSITIVE = 0
     VALUE_NEGATIVE = 0
 
-    IMAGE_SCALE = SCALE(IMAGE, RANGE_IN = [0., 255.], RANGE_OUT = [-SATURATION, SATURATION])
+    IMAGE_SCALE = SCALE(
+                        IMAGE,
+                        RANGE_IN=[0., 255.],
+                        RANGE_OUT=[-SATURATION, SATURATION]
+                        )
 
     SIZE_X, SIZE_Y = IMAGE_SCALE.shape[0], IMAGE_SCALE.shape[1]
 
     for I in range(SIZE_X):
         for J in range(SIZE_Y):
-            if (I-SIZE_X/2) ** 2. + (J-SIZE_Y/2) ** 2. < RSUN ** 2. :
-                if IMAGE_SCALE[I, J] > THRESHOLD :
+            if (I-SIZE_X/2) ** 2. + (J-SIZE_Y/2) ** 2. < RSUN ** 2.:
+                if IMAGE_SCALE[I, J] > THRESHOLD:
                     VALUE_POSITIVE += IMAGE_SCALE[I, J]
-                elif IMAGE_SCALE[I, J] < -THRESHOLD :
+                elif IMAGE_SCALE[I, J] < -THRESHOLD:
                     VALUE_NEGATIVE += IMAGE_SCALE[I, J]
-                else :
+                else:
                     None
 
-    FACT =  (695500./RSUN) * (695500./RSUN) * 1000 * 1000 * 100 * 100
+    FACT = (695500./RSUN) * (695500./RSUN) * 1000 * 1000 * 100 * 100
 
     FLUX_POSITIVE = VALUE_POSITIVE * FACT
     FLUX_NEGATIVE = VALUE_NEGATIVE * FACT
@@ -121,14 +122,13 @@ def TUMF_VALUE(IMAGE, RSUN, SATURATION, THRESHOLD):
     return FLUX_POSITIVE, FLUX_NEGATIVE, FLUX_TOTAL
 
 
-#%%
-
 ITER = DISPLAY_ITER
-while ITER <= MAX_ITER :
+while ITER <= MAX_ITER:
 
-    SITER = '%07d'%ITER
+    SITER = '%07d' % ITER
 
-    MODEL_NAME = './MODELS/' + TRIAL_NAME + '/' + MODE + '/' + MODE + '_ITER' + SITER + '.h5'
+    MODEL_NAME = './MODELS/' + TRIAL_NAME + '/' + MODE + '/' + MODE + '_ITER'
+    + SITER + '.h5'
 
     SAVE_PATH1 = RESULT_PATH1 + 'ITER' + SITER + '/'
     os.mkdir(SAVE_PATH1) if not os.path.exists(SAVE_PATH1) else None
@@ -138,13 +138,12 @@ while ITER <= MAX_ITER :
 
     FIGURE_PATH = FIGURE_PATH_MAIN + 'ITER' + SITER
 
-
     EX = 0
-    while EX < 1 :
+    while EX < 1:
         if os.path.exists(MODEL_NAME):
             print('Starting Iter ' + str(ITER) + ' ...')
             EX = 1
-        else :
+        else:
             print('Waiting Iter ' + str(ITER) + ' ...')
             time.sleep(SLEEP_TIME)
 
@@ -153,13 +152,15 @@ while ITER <= MAX_ITER :
     REAL_A = MODEL.input
     FAKE_B = MODEL.output
     NET_G_GENERATE = K.function([REAL_A], [FAKE_B])
+
     def NET_G_GEN(A):
-        return np.concatenate([NET_G_GENERATE([A[I:I+1]])[0] for I in range(A.shape[0])], axis=0)
+        return np.concatenate([NET_G_GENERATE([A[I:I+1]])[0]
+                              for I in range(A.shape[0])], axis=0)
 
     UTMF_REAL = []
     UTMF_FAKE = []
 
-    for I in range(len(IMAGE_LIST1)) :
+    for I in range(len(IMAGE_LIST1)):
         IMG = np.float32(imread(IMAGE_LIST1[I]) / 255.0 * 2 - 1)
         REAL = np.float32(imread(IMAGE_LIST3[I]))
         DATE = IMAGE_LIST1[I][-19:-4]
@@ -177,7 +178,7 @@ while ITER <= MAX_ITER :
         UTMF_REAL.append(RT)
         UTMF_FAKE.append(FT)
 
-    for J in range(len(IMAGE_LIST2)) :
+    for J in range(len(IMAGE_LIST2)):
         IMG = np.float32(imread(IMAGE_LIST2[J]) / 255.0 * 2 - 1)
         DATE = IMAGE_LIST2[J][-19:-4]
         IMG.shape = (BATCH_SIZE, ISIZE, ISIZE, NC_IN)
@@ -189,13 +190,13 @@ while ITER <= MAX_ITER :
         imsave(SAVE_NAME, FAKE)
 
     def MAKE_FIGURE():
-        INPUT1_IMAGE_LIST = listdir_nohidden('./DATA/TEST/'+INPUT1 +'/')
+        INPUT1_IMAGE_LIST = listdir_nohidden('./DATA/TEST/'+INPUT1 + '/')
         I1 = np.array(imread(INPUT1_IMAGE_LIST[0]))
         I2 = np.array(imread(INPUT1_IMAGE_LIST[1]))
         I3 = np.array(imread(INPUT1_IMAGE_LIST[2]))
         I4 = np.array(imread(INPUT1_IMAGE_LIST[3]))
 
-        OUTPUT_IMAGE_LIST = listdir_nohidden('./DATA/TEST/'+OUTPUT +'/')
+        OUTPUT_IMAGE_LIST = listdir_nohidden('./DATA/TEST/'+OUTPUT + '/')
 
         T1 = np.array(imread(OUTPUT_IMAGE_LIST[0]))
         T2 = np.array(imread(OUTPUT_IMAGE_LIST[1]))
@@ -210,51 +211,51 @@ while ITER <= MAX_ITER :
         fig2 = plt.figure()
 
         ax211 = fig2.add_subplot(3, 4, 1)
-        ax211.imshow(I1, cmap = 'gray')
+        ax211.imshow(I1, cmap='gray')
         ax211.axis('off')
 
         ax212 = fig2.add_subplot(3, 4, 2)
-        ax212.imshow(I2, cmap = 'gray')
+        ax212.imshow(I2, cmap='gray')
         ax212.axis('off')
 
         ax213 = fig2.add_subplot(3, 4, 3)
-        ax213.imshow(I3, cmap = 'gray')
+        ax213.imshow(I3, cmap='gray')
         ax213.axis('off')
 
         ax214 = fig2.add_subplot(3, 4, 4)
-        ax214.imshow(I4, cmap = 'gray')
+        ax214.imshow(I4, cmap='gray')
         ax214.axis('off')
 
         ax221 = fig2.add_subplot(3, 4, 5)
-        ax221.imshow(O1, cmap = 'gray')
+        ax221.imshow(O1, cmap='gray')
         ax221.axis('off')
 
         ax222 = fig2.add_subplot(3, 4, 6)
-        ax222.imshow(O2, cmap = 'gray')
+        ax222.imshow(O2, cmap='gray')
         ax222.axis('off')
 
         ax223 = fig2.add_subplot(3, 4, 7)
-        ax223.imshow(O3, cmap = 'gray')
+        ax223.imshow(O3, cmap='gray')
         ax223.axis('off')
 
         ax224 = fig2.add_subplot(3, 4, 8)
-        ax224.imshow(O4, cmap = 'gray')
+        ax224.imshow(O4, cmap='gray')
         ax224.axis('off')
 
         ax231 = fig2.add_subplot(3, 4, 9)
-        ax231.imshow(T1, cmap = 'gray')
+        ax231.imshow(T1, cmap='gray')
         ax231.axis('off')
 
         ax232 = fig2.add_subplot(3, 4, 10)
-        ax232.imshow(T2, cmap = 'gray')
+        ax232.imshow(T2, cmap='gray')
         ax232.axis('off')
 
         ax233 = fig2.add_subplot(3, 4, 11)
-        ax233.imshow(T3, cmap = 'gray')
+        ax233.imshow(T3, cmap='gray')
         ax233.axis('off')
 
         ax234 = fig2.add_subplot(3, 4, 12)
-        ax234.imshow(T4, cmap = 'gray')
+        ax234.imshow(T4, cmap='gray')
         ax234.axis('off')
 
         fig2.savefig(FIGURE_PATH + '_FIGURE2.png')
@@ -268,15 +269,16 @@ while ITER <= MAX_ITER :
         fig3.savefig(FIGURE_PATH + '_FIGURE3.png')
         plt.close(fig3)
 
-        INPUT2_IMAGE_LIST = listdir_nohidden('./DATA/TEST/'+INPUT2 +'/')
-        INPUT_TRAIN_IMAGE_LIST = listdir_nohidden('./DATA/TRAIN/'+INPUT +'/')
+        INPUT2_IMAGE_LIST = listdir_nohidden('./DATA/TEST/'+INPUT2 + '/')
+        INPUT_TRAIN_IMAGE_LIST = listdir_nohidden('./DATA/TRAIN/'+INPUT + '/')
 
         U1 = np.array(imread(INPUT2_IMAGE_LIST[0]))
         U2 = np.array(imread(INPUT2_IMAGE_LIST[1]))
         U3 = np.array(imread(INPUT_TRAIN_IMAGE_LIST[0]))
         U4 = np.array(imread(INPUT_TRAIN_IMAGE_LIST[1]))
 
-        OUTPUT_TRAIN_IMAGE_LIST = listdir_nohidden('./DATA/TRAIN/'+OUTPUT +'/')
+        OUTPUT_TRAIN_IMAGE_LIST = listdir_nohidden('./DATA/TRAIN/' +
+                                                   OUTPUT + '/')
 
         D1 = np.array(imread(SAVE_PATH2 + '/' + OP2 + '_0.png'))
         D2 = np.array(imread(SAVE_PATH2 + '/' + OP2 + '_1.png'))
@@ -286,37 +288,36 @@ while ITER <= MAX_ITER :
         fig4 = plt.figure()
 
         ax411 = fig4.add_subplot(2, 4, 1)
-        ax411.imshow(U1, cmap = 'gray')
+        ax411.imshow(U1, cmap='gray')
         ax411.axis('off')
 
         ax412 = fig4.add_subplot(2, 4, 2)
-        ax412.imshow(U2, cmap = 'gray')
+        ax412.imshow(U2, cmap='gray')
         ax412.axis('off')
 
         ax413 = fig4.add_subplot(2, 4, 3)
-        ax413.imshow(U3, cmap = 'gray')
+        ax413.imshow(U3, cmap='gray')
         ax413.axis('off')
 
         ax414 = fig4.add_subplot(2, 4, 4)
-        ax414.imshow(U4, cmap = 'gray')
+        ax414.imshow(U4, cmap='gray')
         ax414.axis('off')
 
         ax421 = fig4.add_subplot(2, 4, 5)
-        ax421.imshow(D1, cmap = 'gray')
+        ax421.imshow(D1, cmap='gray')
         ax421.axis('off')
 
         ax422 = fig4.add_subplot(2, 4, 6)
-        ax422.imshow(D2, cmap = 'gray')
+        ax422.imshow(D2, cmap='gray')
         ax422.axis('off')
 
         ax423 = fig4.add_subplot(2, 4, 7)
-        ax423.imshow(D3, cmap = 'gray')
+        ax423.imshow(D3, cmap='gray')
         ax423.axis('off')
 
         ax424 = fig4.add_subplot(2, 4, 8)
-        ax424.imshow(D4, cmap = 'gray')
+        ax424.imshow(D4, cmap='gray')
         ax424.axis('off')
-
 
         fig4.savefig(FIGURE_PATH + '_FIGURE4.png')
         plt.close(fig4)
