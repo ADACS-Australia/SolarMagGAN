24db96ef4c247bca5fb27e684e725ec775372232
Cam <camerontasmith@gmail.com>
2019-07-11 11:28:18 +1000

    added data collector

---
+++ get_sdo_ar_data.py
@@ -0,0 +1,33 @@
+# Python code for retrieving HMI and AIA data
+
+from sunpy.net import Fido, attrs as a
+import drms
+c = drms.Client() 
+from sunpy.net import jsoc
+client = jsoc.JSOCClient() 
+import astropy.units as u # for AIA
+import pickle # for saving query to file
+
+# can make multiple queries and save the details to files based on the AR and retrieve the data later
+time_ar=['2019-03-07T19:00:00','2019-03-07T19:30:00']  # AR NOAA 12734
+
+res_ic = Fido.search(a.Time(time_ar[0],time_ar[1]), a.jsoc.Series('hmi.ic_45s'),a.jsoc.Notify('daniela.lacatus@monash.edu'))
+res_m = Fido.search(a.Time(time_ar[0],time_ar[1]), a.jsoc.Series('hmi.m_45s'),a.jsoc.Notify('daniela.lacatus@monash.edu'))
+res_304 = Fido.search(a.Time(time_ar[0],time_ar[1]),a.jsoc.Notify('daniela.lacatus@monash.edu'), a.jsoc.Series('aia.lev1_euv_12s'),a.jsoc.Segment('image') ,a.jsoc.Wavelength(304*u.AA))
+
+# save the the query details to file 
+with open('ar1.pkl', 'wb') as f: 
+    pickle.dump([time_ar,res_ic,res_m,res_304], f)
+
+
+# Wait about 15 minutes for the requests to be processed
+# if starting a new python session load the packages above
+
+# load query info
+with open('ar1.pkl','rb') as f: 
+    time_ar,res_ic,res_m,res_304 = pickle.load(f)
+
+# download data
+downloaded_files = Fido.fetch(res_ic,path='./ar1/')  # wait before running this command, as request nets to be processed
+downloaded_files = Fido.fetch(res_m,path='./ar1/')  # wait before running this command, as request nets to be processed
+downloaded_files = Fido.fetch(res_304,path='./ar1/') # wait before running this command, as request nets to be processed
