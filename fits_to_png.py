66e989e4e833737a5afd193d47f21c268014b3d2
Cam <camerontasmith@gmail.com>
2019-07-15 14:45:03 +1000

    changed range to allow negative values

--- fits_to_png.py
+++ fits_to_png.py
@@ -8,13 +8,13 @@ output = 'HMI'
 w = h = 1024  # desired width and height of png
 m_max = 1500  # maximum value for magnetograms
 m_min = -1500  # minimum value for magnetograms
-a_min = 0
+a_min = -10
 a_max = 150
 
 
 def resize_and_save_to_png(name, fits_path, png_path, min, max, w, h):
     print(name)
-    hdul = fits.open(fits_path + name + ".fits", ext=0)
+    hdul = fits.open(fits_path + name + ".fits", memmap=True, ext=0)
     hdul.verify("fix")
     image_data = hdul[1].data
     # clip data between (min, max):
@@ -25,7 +25,7 @@ def resize_and_save_to_png(name, fits_path, png_path, min, max, w, h):
     image_data = image_data/(max - min)
 
     # format data, and convert to image
-    image = Image.fromarray(np.uint8(image_data * 255) , 'L')
+    image = Image.fromarray(np.uint8(image_data * 255), 'L')
     image = image.resize((w, h), Image.LANCZOS)
     # flip image to match original orientation.
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
