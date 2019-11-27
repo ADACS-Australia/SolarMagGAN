7f9eb75ac0608eaa2d9f8ae8388b498db19bb303
chemron <camerontasmith@gmail.com>
2019-11-27 11:55:00 +1100

    created stereo data fetch file for specific dates

--- get_stereo_data.py
+++ get_stereo_data.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 import urllib.request
-f = open('FITS_DATA/STEREO/test.fits', "wb")
-data = urllib.request.urlopen("https://stereo-ssc.nascom.nasa.gov/data" +
-                              "/ins_data/secchi/L0/a/img/euvi/20090116" +
-                              "/20090116_000530_n4euA.fts")
-f.write(data.read())
-f.close()
+# first part of url before date
+url_1 = "https://stereo-ssc.nascom.nasa.gov/data/ins_data/secchi/L0/a/img/euvi/"
+# second part of url after date
+url_2 = "_115615_n4euA.fts"
+
+dates = ["20140604", "20140605", "20140606", "20140607", "20140608", "20140609", "20140610"]
+
+for date in dates:
+    fname = date + url_2 # filename
+    url = url_1 + date + "/" + fname
+    f = open('FITS_DATA/STEREO/' + fname , "wb")
+    data = urllib.request.urlopen(url)
+    f.write(data.read())
+    f.close()
