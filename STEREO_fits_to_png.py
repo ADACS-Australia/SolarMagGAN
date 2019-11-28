684f95f6472fc2ef0931e3e63ec31c3157a6dc5e
Cam <camerontasmith@gmail.com>
2019-11-28 14:22:52 +1100

    finished stereo fits to png conversion script, so the output matches AIA data

--- STEREO_fits_to_png.py
+++ STEREO_fits_to_png.py
@@ -1,22 +1,67 @@
 #!/usr/bin/env python3
-from fits_to_png import save_to_png
 import os
 import sunpy
 import sunpy.map
+from astropy.io import fits
+import numpy as np
 import astropy.units as u
 from astropy.coordinates import SkyCoord
+from PIL import Image
+
+
+def save_to_png(name, fits_path, png_path, min, max, w, h, top_right=None,
+                bottom_left=None):
+
+    print(name)
+    filename = fits_path + name + ".fits"
+    filename = fits_path + name + ".fits"
+    hdul = fits.open(filename, memmap=False, ext=0)
+    hdul.verify("fix")
+    image_data = hdul[0].data
+
+    # find median
+    med = np.median(image_data)
+
+    # Cropping to desired range
+    map = sunpy.map.Map(filename)
+    image_data = map.submap(bottom_left, top_right).data
+
+    # translate data so it starts at 0
+    image_data -= min
+
+    # normalise to keep consistant
+    image_data = image_data/(med-min)
+
+    # clip data between (0, (max - min)):
+    image_data = np.clip(image_data, 0, (max - min))
+
+    # normalise data between 0 and 1
+    image_data = image_data/((max - min))
+
+    # format data, and convert to image
+    image = Image.fromarray(np.uint8(image_data * 255), 'L')
+    # crop to diameter of sun
+    image = image.resize((w, h), Image.LANCZOS)
+    # flip image to match original orientation.
+    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    # rotate images to match
+
+    image.save(png_path + name + ".png")
+
 
 data = "STEREO"
 w = h = 1024
-min = 0
-max = 150/4
+# min and maxed were chosen so that the result was as similar to
+# aia between a min of 0 and a max of 150
+min = 700
+max = 1200
 fits_path = "FITS_DATA/STEREO/"
 png_path = "DATA/TEST/STEREO/"
 filename = "./" + fits_path + os.listdir(fits_path)[0]
 map_ref = sunpy.map.Map(filename)
-top_right = SkyCoord(1000 * u.arcsec, 1000 * u.arcsec,
+top_right = SkyCoord(920 * u.arcsec, 920 * u.arcsec,
                      frame=map_ref.coordinate_frame)
-bottom_left = SkyCoord(-1000 * u.arcsec, -1000 * u.arcsec,
+bottom_left = SkyCoord(-920 * u.arcsec, -920 * u.arcsec,
                        frame=map_ref.coordinate_frame)
 
 for filename in os.listdir(fits_path):
@@ -28,60 +73,6 @@ for filename in os.listdir(fits_path):
                 max=max,
                 w=w,
                 h=h,
-                normalise=True,
-                crop=True,
                 top_right=top_right,
                 bottom_left=bottom_left,
-                index=0
                 )
-def save_to_png(name, fits_path, png_path, min, max, w, h,
-                normalise=False, rotate=False, abs=False,
-                crop=False, top_right=None, bottom_left=None,
-                index=1):
-    print(name)
-    filename = fits_path + name + ".fits"
-    hdul = fits.open(filename, memmap=index, ext=0)
-    hdul.verify("fix")
-    image_data = hdul[index].data
-
-    # find median before cropping
-    if normalise and index == 1:
-        med = hdul[index].header['DATAMEDN']
-    elif normalise and index == 0:
-        med = np.median(image_data)
-
-    if abs:
-        image_data = np.abs(image_data)
-        min = np.max([0, min])
-
-    if crop:
-        # Cropping to desired range
-        map = sunpy.map.Map(filename)
-        if rotate:
-            map = map.rotate(angle=180 * u.deg)
-
-        # Cropping
-        image_data = map.submap(bottom_left, top_right).data
-
-    # normalise
-    if normalise:
-        image_data = image_data/med
-
-    # clip data between (min, max):
-    image_data = np.clip(image_data, min, max)
-    # translate data so it's between (0, max-min):
-    image_data -= min
-    # normalise data
-    image_data = image_data/(max - min)
-
-    # format data, and convert to image
-    image = Image.fromarray(np.uint8(image_data * 255), 'L')
-    # crop to diameter of sun
-    image = image.resize((w, h), Image.LANCZOS)
-    # flip image to match original orientation.
-    image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    # rotate images to match
-    if rotate and not crop:
-        image = image.transpose(Image.ROTATE_180)
-
-    image.save(png_path + name + ".png")
