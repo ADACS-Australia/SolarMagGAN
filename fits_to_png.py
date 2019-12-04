01c8451b6ab137ab54be6725e5709873db1c4b03
Cam <camerontasmith@gmail.com>
2019-12-04 14:23:33 +1100

    cleaned up and added argument parsing

--- fits_to_png.py
+++ fits_to_png.py
@@ -6,14 +6,42 @@ import sunpy
 import sunpy.map
 import astropy.units as u
 from astropy.coordinates import SkyCoord
+import argparse
+
+# parse the optional arguments:
+parser = argparse.ArgumentParser()
+parser.add_argument("--min",
+                    help="lower bound cutoff pixel value for AIA",
+                    type=int,
+                    default=0
+                    )
+parser.add_argument("--max",
+                    help="upper bound cutoff pixel value for AIA",
+                    type=int,
+                    default=150/4
+                    )
+
+parser.add_argument("--random",
+                    help="randomly assign upper bound pixel value for AIA",
+                    action="store_true"
+                    )
+
+parser.add_argument("--name",
+                    help="name of folder for AIA to be saved in",
+                    default="AIA")
+
+args = parser.parse_args()
+
 
 input = 'AIA'
 output = 'HMI'
 w = h = 1024  # desired width and height of png
 m_max = 100  # maximum value for magnetograms
 m_min = -100  # minimum value for magnetograms
-a_min = 0
-a_max = 150/4
+a_min = args.min
+a_max = args.max
+AIA = True
+HMI = True
 
 
 def save_to_png(name, fits_path, png_path, min, max, w, h,
@@ -61,39 +89,9 @@ def save_to_png(name, fits_path, png_path, min, max, w, h,
     image.save(png_path + name + ".png")
 
 
-def main(data, min, max, w, h, normalise=False, rotate=False,
-         abs=False, crop=False, top_right=None, bottom_left=None):
-    fits_path = "FITS_DATA/" + data + '/'
-    if abs:
-        data = 'ABS_' + data
-    for filename in os.listdir(fits_path):
-        file_info = filename.split('.')
-        date = file_info[2].replace('-', '')
-        month = date[4:6]
-        if month == '09' or month == '10':
-            png_path = "DATA/TEST/" + data + '/'
-        else:
-            png_path = "DATA/TRAIN/" + data + '/'
-        os.makedirs(png_path) if not os.path.exists(png_path) else None
-        save_to_png(name=filename[:-5],
-                    fits_path=fits_path,
-                    png_path=png_path,
-                    min=min,
-                    max=max,
-                    w=w,
-                    h=h,
-                    normalise=normalise,
-                    rotate=rotate,
-                    abs=abs,
-                    crop=crop,
-                    top_right=top_right,
-                    bottom_left=bottom_left
-                    )
-
-
 if __name__ == "__main__":
     # -1000 to 1000 arcsec results in a full disk image.
-    # Slightly lower values will be closerto the actual limb of the Sun
+    # Slightly lower values will be closerto the actual edge of the Sun
     filename = "./FITS_DATA/HMI/" + os.listdir("FITS_DATA/HMI/")[0]
     map_ref = sunpy.map.Map(filename)
     top_right = SkyCoord(1000 * u.arcsec, 1000 * u.arcsec,
@@ -101,24 +99,62 @@ if __name__ == "__main__":
     bottom_left = SkyCoord(-1000 * u.arcsec, -1000 * u.arcsec,
                            frame=map_ref.coordinate_frame)
     # AIA:
-    main(data=input,
-         min=a_min,
-         max=a_max,
-         w=w,
-         h=h,
-         normalise=True,
-         crop=True,
-         top_right=top_right,
-         bottom_left=bottom_left
-         )
+    if AIA:
+        fits_path = "FITS_DATA/" + input + '/'
+        name = args.name
+        test_path = "DATA/TEST/" + name + '/'
+        train_path = "DATA/TRAIN/" + name + '/'
+        # make directories if they don't exist
+        os.makedirs(test_path) if not os.path.exists(test_path) else None
+        os.makedirs(train_path) if not os.path.exists(train_path) else None
+
+        for filename in os.listdir(fits_path):
+            file_info = filename.split('.')
+            date = file_info[2].replace('-', '')
+            month = date[4:6]
+            if month == '09' or month == '10':
+                png_path = test_path
+            else:
+                png_path = train_path
+            save_to_png(name=filename[:-5],
+                        fits_path=fits_path,
+                        png_path=png_path,
+                        min=a_min,
+                        max=a_max,
+                        w=w,
+                        h=h,
+                        normalise=True,
+                        crop=True,
+                        top_right=top_right,
+                        bottom_left=bottom_left
+                        )
     # HMI:
-    main(data=output,
-         min=m_min,
-         max=m_max,
-         w=w,
-         h=h,
-         rotate=True,
-         crop=True,
-         top_right=top_right,
-         bottom_left=bottom_left
-         )
+    if HMI:
+        fits_path = "FITS_DATA/" + output + '/'
+        test_path = "DATA/TEST/" + output + '/'
+        train_path = "DATA/TRAIN/" + output + '/'
+        # make directories if they don't exist
+        os.makedirs(test_path) if not os.path.exists(test_path) else None
+        os.makedirs(train_path) if not os.path.exists(train_path) else None
+
+        for filename in os.listdir(fits_path):
+            file_info = filename.split('.')
+            date = file_info[2].replace('-', '')
+            month = date[4:6]
+            if month == '09' or month == '10':
+                png_path = test_path
+            else:
+                png_path = train_path
+            save_to_png(name=filename[:-5],
+                        fits_path=fits_path,
+                        png_path=png_path,
+                        min=m_min,
+                        max=m_max,
+                        w=w,
+                        h=h,
+                        rotate=True,
+                        abs=abs,
+                        crop=True,
+                        top_right=top_right,
+                        bottom_left=bottom_left
+                        )
