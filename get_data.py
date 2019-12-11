3937f62e6bb9d64b730115043f2e66bec43cbfca
Cam <camerontasmith@gmail.com>
2019-12-11 16:49:58 +1100

    added argument parsing

--- get_data.py
+++ get_data.py
@@ -4,25 +4,77 @@ from sunpy.net import Fido, attrs as a
 import astropy.units as u  # for AIA
 import pickle  # for saving query to file
 import os
+import argparse
 # TODO: do many short queryies rather than one large one
 email = 'csmi0005@student.monash.edu'
 # can make multiple queries and save the details to files based on the AR and
 # retrieve the data later
 
-# query duration:
-AIA_start = HMI_start = '2015/01/01 00:00:00'
-AIA_end = HMI_end = '2015/01/01 00:00:00'
-STEREO_start = '2012-01-01'
-STEREO_end = '2012-01-10'
+parser = argparse.ArgumentParser()
+parser.add_argument("--AIA",
+                    help="download AIA if set",
+                    action="store_true"
+                    )
+parser.add_argument("--HMI",
+                    help="download HMI if set",
+                    action="store_true"
+                    )
+parser.add_argument("--STEREO",
+                    help="download STEREO if set",
+                    action="store_true"
+                    )
+parser.add_argument("--start",
+                    help="start date for AIA and HMI",
+                    default='2011/01/01 00:00:00')
+parser.add_argument("--end",
+                    help="end date for AIA and HMI",
+                    default='2017/01/01 00:00:00')
 
-AIA = False
-AIA_path = './FITS_DATA/AIA'
-HMI = False
-HMI_path = './FITS_DATA/HMI'
-STEREO = True
-STEREO_path = './FITS_DATA/STEREO'
-cadence = 12*u.hour  # take images every 12 hours
+parser.add_argument("--cadence",
+                    help="AIA and HMI cadence in hours",
+                    type=int,
+                    default=12
+                    )
+parser.add_argument("--AIA_path",
+                    help="directory to store AIA data",
+                    default='./FITS_DATA/AIA'
+                    )
+parser.add_argument("--HMI_path",
+                    help="directory to store HMI data",
+                    default='./FITS_DATA/HMI'
+                    )
+parser.add_argument("--STEREO_path",
+                    help="directory to store STEREO data",
+                    default='./FITS_DATA/STEREO'
+                    )
+parser.add_argument("--STEREO_start",
+                    help="start time for STEREO",
+                    default='2011/01/01 00:00:00')
+parser.add_argument("--STEREO_end",
+                    help="end date for STEREO",
+                    default='2017/01/01 00:00:00')
+parser.add_argument("--STEREO_cadence",
+                    help="download 1 in every n images",
+                    type=int,
+                    default=100
+                    )
+args = parser.parse_args()
+
+# query duration:
+AIA_start = HMI_start = args.start
+AIA_end = HMI_end = args.end
+STEREO_start = args.STEREO_start
+STEREO_end = args.STEREO_end
+
+AIA = args.AIA
+AIA_path = args.AIA_path
+HMI = args.HMI
+HMI_path = args.HMI_path
+STEREO = args.STEREO
+STEREO_path = args.STEREO_path
+cadence = args.cadence*u.hour  # take images every 12 hours
 wavelength = 304*u.AA  # 304 Angstroms
+stereo_cadence = args.STEREO_cadence  # use 1 in every n images
 
 if AIA:
     res_aia = Fido.search(a.Time(AIA_start, AIA_end),
